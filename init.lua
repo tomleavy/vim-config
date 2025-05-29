5b997d79a484ba3967f7b404b16d6abbcc37e2e1
Tom Leavy <tomleavy@amazon.com>
2025-05-29 18:00:34 -0400

    fix js handling

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ vim.opt.shiftwidth = 4
 vim.opt.smarttab = true
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = {"typescript", "javascript", "json", "lua"},
+  pattern = {"typescript", "javascript", "json", "lua", "javascriptreact", "typescriptreact"},
   callback = function()
     vim.opt_local.tabstop = 2
     vim.opt_local.softtabstop = 0
